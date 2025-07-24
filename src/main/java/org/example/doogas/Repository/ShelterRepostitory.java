4f61070868253093e434a7a75620783b6acddeff
Kosta <prc.zrp@mail.ru>
2025-07-24 15:10:49 +0300

    sec

--- src/main/java/org/example/doogas/Repository/ShelterRepostitory.java
+++ src/main/java/org/example/doogas/Repository/ShelterRepostitory.java
@@ -2,11 +2,7 @@ package org.example.doogas.Repository;
 
 
 import org.example.doogas.Model.Dog;
-import org.example.doogas.State.DogState;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
